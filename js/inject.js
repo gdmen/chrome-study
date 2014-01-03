e82d5b6b0e4e3fed667c40247aba7b2fdf6d1a8d
unknown <gmenezes@seas.upenn.edu>
2014-01-02 19:27:07 -0500

    Page scales to sidebar size.

--- js/inject.js
+++ js/inject.js
@@ -1,24 +1,37 @@
 (function () {
   DIV_ID = "study-guide-sidebar";
+  PAGE_ID = "study-guide-page";
   if (!document.getElementById(DIV_ID)) {
     console.log("inject.js start");
+    // Inject container
+    $('body').wrapInner('<div id="' + PAGE_ID + '"/>');
+    
     iframe = document.createElement('iframe');
     iframe.src = chrome.extension.getURL('sidebar.html');
     resizable_div = document.createElement("div");
     resizable_div.id = DIV_ID;
     //resizable_div.appendChild(iframe);
     document.body.appendChild(resizable_div);
-    console.log("inject.js resizable");
+    resizePage();
     $('#study-guide-sidebar').resizable({
       handles: 'n,w,s,e',
       minWidth: 200,
       maxWidth: 500,
       resize: function(event, ui) {
         $(this).css({left:''});
+        resizePage();
       }
     });
   } else {
-    div = document.getElementById(DIV_ID);
-    div.parentNode.removeChild(div);
+    $(DIV_ID).remove();
+  }
+  function resizePage() {
+    console.log("resizePage");
+    console.log($('body').innerWidth());
+    console.log($('#' + DIV_ID));
+    console.log($('#' + DIV_ID).outerWidth());
+    console.log($('body').innerWidth() - $('#' + DIV_ID).outerWidth());
+    $('#' + PAGE_ID).css({width: $('body').innerWidth() - $('#' + DIV_ID).outerWidth()});
+    console.log($('#' + PAGE_ID).css('width'));
   }
 })()
