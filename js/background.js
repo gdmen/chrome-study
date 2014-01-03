e89009f24d2eec34b41b40855f54ae25db952e24
unknown <gmenezes@seas.upenn.edu>
2014-01-03 00:48:00 -0500

    Toggle sidebar on ALL tabs.

--- js/background.js
+++ js/background.js
@@ -1,7 +1,80 @@
+function isset(key) {
+  return !(localStorage.getItem(key) === null);
+}
+/*
+ * Sets to local storage
+ * @params:
+ *    - key
+ *    - value
+ * @returns:
+ *    none
+ */
+function setLocal(key, value){
+  localStorage[key] = value;
+}
+/*
+ * Gets from local storage
+ * @params:
+ *    - key
+ * @returns:
+ *    - value on presence
+ *    - undefined on absence
+ */
+function getLocal(key){
+  return JSON.parse(localStorage[key]);
+}
+
+TOGGLE = 'study-guide-toggle';
+if(!isset(TOGGLE)) {
+  console.log("not set");
+  setLocal(TOGGLE, false);
+  refreshAllTabs(getLocal(TOGGLE));
+}
+
 chrome.browserAction.onClicked.addListener(function(e) {
-  chrome.tabs.insertCSS(null, {file: "css/ui-lightness/jquery-ui.min.css"});
-  chrome.tabs.insertCSS(null, {file: "css/inject.css"});
-  chrome.tabs.executeScript(null, {file: "js/libs/jquery.min.js"});
-  chrome.tabs.executeScript(null, {file: "js/libs/jquery-ui.min.js"});
-  chrome.tabs.executeScript(null, {file: "js/inject.js"});
+  toggle = getLocal(TOGGLE);
+  if(toggle) {
+    setLocal(TOGGLE, false);
+  } else {
+    setLocal(TOGGLE, true);
+  }
+  toggle = getLocal(TOGGLE);
+  console.log("Toggle onClicked " + toggle);
+  refreshAllTabs(toggle);
 });
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  toggle = getLocal(TOGGLE);
+  console.log("Toggle onUpdated " + toggle);
+  refreshTab(tab, toggle)
+});
+
+chrome.tabs.onCreated.addListener(function(tab) {
+  toggle = getLocal(TOGGLE);
+  console.log("Toggle onCreated " + toggle);
+  refreshTab(tab, toggle)
+});
+
+function refreshTab(tab, toggle) {
+  if(toggle) {
+    chrome.tabs.insertCSS(tab.id, {file: "css/ui-lightness/jquery-ui.min.css"});
+    chrome.tabs.insertCSS(tab.id, {file: "css/inject.css"});
+    chrome.tabs.sendMessage(tab.id, {greeting: "on"});
+  } else {
+    chrome.tabs.sendMessage(tab.id, {greeting: "off"});
+  }
+}
+
+function refreshAllTabs(toggle) {
+  console.log("refreshTabs: " + toggle);
+  chrome.tabs.query({active: true}, function(tabs) {
+    for (var i = 0; i < tabs.length; i++) {
+      refreshTab(tabs[i], toggle);
+    }
+  });
+  chrome.tabs.query({active: false}, function(tabs) {
+    for (var i = 0; i < tabs.length; i++) {
+      refreshTab(tabs[i], toggle);
+    }
+  });
+}
--- js/inject.js
+++ js/inject.js
@@ -1,37 +1,44 @@
-(function () {
-  DIV_ID = "study-guide-sidebar";
-  PAGE_ID = "study-guide-page";
-  if (!document.getElementById(DIV_ID)) {
-    console.log("inject.js start");
-    // Inject container
-    $('body').wrapInner('<div id="' + PAGE_ID + '"/>');
-    
-    iframe = document.createElement('iframe');
-    iframe.src = chrome.extension.getURL('sidebar.html');
-    resizable_div = document.createElement("div");
-    resizable_div.id = DIV_ID;
-    //resizable_div.appendChild(iframe);
-    document.body.appendChild(resizable_div);
-    resizePage();
-    $('#study-guide-sidebar').resizable({
-      handles: 'w',
-      minWidth: 300,
-      resize: function(event, ui) {
-        $(this).css({left:''});
-        resizePage();
-      }
-    });
+DIV_ID = "study-guide-sidebar";
+PAGE_ID = "study-guide-page";
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if(request.greeting == "on") {
+    on();
+  } else if(request.greeting == "off") {
+    off();
   } else {
-    $('#' + DIV_ID).remove();
+    console.log(request.greeting);
   }
-  function resizePage() {
-    /*console.log("resizePage");
-    console.log($('body').innerWidth());
-    console.log($('#' + DIV_ID));
-    console.log($('#' + DIV_ID).outerWidth());
-    console.log($('body').innerWidth() - $('#' + DIV_ID).outerWidth());
-    */
-    $('#' + PAGE_ID).css({width: $('body').innerWidth() - $('#' + DIV_ID).outerWidth()});
-    //console.log($('#' + PAGE_ID).css('width'));
-  }
-})()
+});
+
+function on() {
+  console.log("on");
+  // Inject container
+  $('body').wrapInner('<div id="' + PAGE_ID + '"/>');
+  
+  iframe = document.createElement('iframe');
+  iframe.src = chrome.extension.getURL('sidebar.html');
+  resizable_div = document.createElement("div");
+  resizable_div.id = DIV_ID;
+  //resizable_div.appendChild(iframe);
+  document.body.appendChild(resizable_div);
+  resizePage();
+  $('#study-guide-sidebar').resizable({
+    handles: 'w',
+    minWidth: 300,
+    resize: function(event, ui) {
+      $(this).css({left:''});
+      resizePage();
+    }
+  });
+}
+
+function off() {
+  console.log("off");
+  $('#' + DIV_ID).remove();
+  $('#' + PAGE_ID).replaceWith($('#' + PAGE_ID).contents());
+}
+
+function resizePage() {
+  $('#' + PAGE_ID).css({width: $('body').innerWidth() - $('#' + DIV_ID).outerWidth()});
+}
