1a972a8a4cb96dd2eb21ef47361896b6dee5627a
unknown <gmenezes@seas.upenn.edu>
2014-01-03 01:02:49 -0500

    Changed to single tab toggling.

--- js/background.js
+++ js/background.js
@@ -1,80 +1,39 @@
-function isset(key) {
-  return !(localStorage.getItem(key) === null);
-}
-/*
- * Sets to local storage
- * @params:
- *    - key
- *    - value
- * @returns:
- *    none
- */
-function setLocal(key, value){
-  localStorage[key] = value;
-}
-/*
- * Gets from local storage
- * @params:
- *    - key
- * @returns:
- *    - value on presence
- *    - undefined on absence
- */
-function getLocal(key){
-  return JSON.parse(localStorage[key]);
-}
-
-TOGGLE = 'study-guide-toggle';
-if(!isset(TOGGLE)) {
-  console.log("not set");
-  setLocal(TOGGLE, false);
-  refreshAllTabs(getLocal(TOGGLE));
-}
-
 chrome.browserAction.onClicked.addListener(function(e) {
-  toggle = getLocal(TOGGLE);
-  if(toggle) {
-    setLocal(TOGGLE, false);
-  } else {
-    setLocal(TOGGLE, true);
-  }
-  toggle = getLocal(TOGGLE);
-  console.log("Toggle onClicked " + toggle);
-  refreshAllTabs(toggle);
+  console.log("onClicked");
+  toggleActiveTab();
 });
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  toggle = getLocal(TOGGLE);
-  console.log("Toggle onUpdated " + toggle);
-  refreshTab(tab, toggle)
+  console.log("onUpdated");
+  setActiveTab();
 });
 
 chrome.tabs.onCreated.addListener(function(tab) {
-  toggle = getLocal(TOGGLE);
-  console.log("Toggle onCreated " + toggle);
-  refreshTab(tab, toggle)
+  console.log("onCreated");
+  setActiveTab();
 });
 
-function refreshTab(tab, toggle) {
-  if(toggle) {
-    chrome.tabs.insertCSS(tab.id, {file: "css/ui-lightness/jquery-ui.min.css"});
-    chrome.tabs.insertCSS(tab.id, {file: "css/inject.css"});
-    chrome.tabs.sendMessage(tab.id, {greeting: "on"});
-  } else {
-    chrome.tabs.sendMessage(tab.id, {greeting: "off"});
-  }
+function setActiveTab() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {greeting: "set"});
+  });
 }
 
-function refreshAllTabs(toggle) {
-  console.log("refreshTabs: " + toggle);
-  chrome.tabs.query({active: true}, function(tabs) {
-    for (var i = 0; i < tabs.length; i++) {
-      refreshTab(tabs[i], toggle);
-    }
-  });
-  chrome.tabs.query({active: false}, function(tabs) {
-    for (var i = 0; i < tabs.length; i++) {
-      refreshTab(tabs[i], toggle);
-    }
+function toggleActiveTab() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {greeting: "toggle"});
   });
 }
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if(request.greeting == "inject") {
+    console.log("INJECTING");
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.insertCSS(tabs[0].id, {file: "css/ui-lightness/jquery-ui.min.css"});
+      chrome.tabs.insertCSS(tabs[0].id, {file: "css/inject.css"});
+    });
+    sendResponse({farewell: ""});
+  } else {
+    console.log(request.greeting);
+  }
+});
--- js/inject.js
+++ js/inject.js
@@ -1,35 +1,68 @@
 DIV_ID = "study-guide-sidebar";
 PAGE_ID = "study-guide-page";
 
+STATE_KEY = 'study-guide-toggle-';
+
+if(!isset(STATE_KEY)) {
+  console.log("not set");
+  setLocal(STATE_KEY, false);
+  setSidebar();
+}
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if(request.greeting == "on") {
-    on();
-  } else if(request.greeting == "off") {
-    off();
+  if(request.greeting == "toggle") {
+    console.log("toggle-msg");
+    toggle();
+  } else if(request.greeting == "set") {
+    console.log("set-msg");
+    setSidebar();
   } else {
     console.log(request.greeting);
   }
 });
 
+function toggle() {
+  console.log("toggle");
+  var enabled = getLocal(STATE_KEY);
+  if(enabled) {
+    setLocal(STATE_KEY, false);
+  } else {
+    setLocal(STATE_KEY, true);
+  }
+  setSidebar();
+}
+
+function setSidebar() {
+  var enabled = getLocal(STATE_KEY);
+  if(enabled) {
+    on();
+  } else {
+    off();
+  }
+}
+
 function on() {
   console.log("on");
-  // Inject container
-  $('body').wrapInner('<div id="' + PAGE_ID + '"/>');
-  
-  iframe = document.createElement('iframe');
-  iframe.src = chrome.extension.getURL('sidebar.html');
-  resizable_div = document.createElement("div");
-  resizable_div.id = DIV_ID;
-  //resizable_div.appendChild(iframe);
-  document.body.appendChild(resizable_div);
-  resizePage();
-  $('#study-guide-sidebar').resizable({
-    handles: 'w',
-    minWidth: 300,
-    resize: function(event, ui) {
-      $(this).css({left:''});
-      resizePage();
-    }
+  chrome.runtime.sendMessage({greeting: "inject"}, function(response) {
+    // Inject container
+    console.log("injecting?");
+    $('body').wrapInner('<div id="' + PAGE_ID + '"/>');
+    
+    iframe = document.createElement('iframe');
+    iframe.src = chrome.extension.getURL('sidebar.html');
+    resizable_div = document.createElement("div");
+    resizable_div.id = DIV_ID;
+    //resizable_div.appendChild(iframe);
+    document.body.appendChild(resizable_div);
+    resizePage();
+    $('#study-guide-sidebar').resizable({
+      handles: 'w',
+      minWidth: 300,
+      resize: function(event, ui) {
+        $(this).css({left:''});
+        resizePage();
+      }
+    });
   });
 }
 
@@ -41,4 +74,30 @@ function off() {
 
 function resizePage() {
   $('#' + PAGE_ID).css({width: $('body').innerWidth() - $('#' + DIV_ID).outerWidth()});
+}
+
+function isset(key) {
+  return !(localStorage.getItem(key) === null);
+}
+/*
+ * Sets to local storage
+ * @params:
+ *    - key
+ *    - value
+ * @returns:
+ *    none
+ */
+function setLocal(key, value){
+  localStorage[key] = value;
+}
+/*
+ * Gets from local storage
+ * @params:
+ *    - key
+ * @returns:
+ *    - value on presence
+ *    - undefined on absence
+ */
+function getLocal(key){
+  return JSON.parse(localStorage[key]);
 }
